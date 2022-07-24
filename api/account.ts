20476c953b592396839fd99cf34a58cd39aa89d2
Uditt Lamba <uditt.lamba@pm.me>
2022-07-24 06:57:36 +0530

    added stages

--- api/account.ts
+++ api/account.ts
@@ -2,16 +2,16 @@ import * as express from 'express';
 import * as serverless from 'serverless-http';
 const app = express()
 
-app.get('/accounts/fetch', function (req, res) {
+app.get(`/${process.env.STAGE}/accounts/fetch`, function (req, res) {
     res.send('accounts get response')
 })
-app.post('/accounts/create', function (req, res) {
+app.post(`/${process.env.STAGE}/accounts/create`, function (req, res) {
     res.send('accounts post response')
 })
-app.patch('/accounts/patch', function (req, res) {
+app.patch(`/${process.env.STAGE}/accounts/patch`, function (req, res) {
     res.send('accounts patch response')
 })
-app.delete('/accounts/delete', function (req, res) {
+app.delete(`/${process.env.STAGE}/accounts/delete`, function (req, res) {
     res.send('accounts delete response')
 })
 
